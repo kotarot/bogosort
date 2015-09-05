19f6b6c8b8ba996adb8ddc547d88350656e2296a
Kotaro Terada <tera@toki.waseda.jp>
2015-09-05 15:55:44 +0900

    First version.

---
+++ bogo.js
@@ -0,0 +1,68 @@
+// bogo.js
+
+var ARRAY_SIZE = 5;
+
+// From: http://bost.ocks.org/mike/shuffle/
+var shuffle = function(array) {
+    var m = array.length, t, i;
+
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
+}
+
+// Check if two arrays are same
+var equals = function(a1, a2) {
+    var len1 = a1.length, len2 = a2.length;
+    if (len1 != len2) {
+        return false;
+    }
+    for (var i = 0; i < len1; i++) {
+        if (a1[i] != a2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+var array_to_str = function(array) {
+    return '[' + array.join(', ') + ']';
+}
+
+var sleep = function(time) {
+    var d1 = new Date().getTime(), d2 = new Date().getTime();
+    while (d2 < d1 + time) {
+        d2 = new Date().getTime();
+     }
+}
+
+$(document).ready(function() {
+    // Generate a sorted and suffled array.
+    var answer = new Array(ARRAY_SIZE);
+    for (var i = 0; i < ARRAY_SIZE; i++) {
+        answer[i] = i + 1;
+    }
+
+    // Bogo sort
+    var test = answer.concat();
+    var bogo = setInterval(function() {
+        shuffle(test);
+        $('#sorting').text(array_to_str(test));
+        $('#sorting').fadeIn(500, function() {
+            if (!equals(test, answer)) {
+                $('#sorting').addClass('array-neg');
+                $('#sorting').fadeOut(1000, function() {
+                    $('#sorting').removeClass('array-neg');
+                });
+            } else {
+                clearInterval(bogo);
+                $('#message').show();
+            }
+        });
+    }, 2000);
+});
